defe4ac9b470373c07ab17abcc3eaab1eff60d02
Adam Ratcliffe <adam.ratcliffe@gmail.com>
2014-07-31 14:41:46 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+#Leaflet.twofingerzoom
+======================
+
+An interaction handler that allows the map to be zoomed out on touch screen devices by tapping the screen with two fingers simultaneously.
+
+##Usage
+
+Two finger zoom is enabled automatically on touch screen devices simply through including the plugin or can be controlled explicitly with the `twoFingerZoom` map option.
+
+````javascript
+var map = L.map('map', {
+    twoFingerZoom: true
+ });
+````
+
+##License
+This software is released under the [MIT licence](http://www.opensource.org/licenses/mit-license.php).
---
+++ dist/leaflet.twofingerzoom.js
@@ -0,0 +1,5 @@
+/*
+	Leaflet.twofingerzoom, enables zooming the map out with a two finger tap on touch devices
+	(c) 2014, Adam Ratcliffe, TomTom International BV
+*/
+L.Map.mergeOptions({twoFingerZoom:L.Browser.touch&&!L.Browser.android23});L.Map.TwoFingerZoom=L.Handler.extend({statics:{ZOOM_OUT_THRESHOLD:100},_touchStartTime:[],addHooks:function(){L.DomEvent.on(this._map._container,"touchstart",this._onTouchStart,this);L.DomEvent.on(this._map._container,"touchend",this._onTouchEnd,this)},removeHooks:function(){L.DomEvent.off(this._map._container,"touchstart",this._onTouchStart,this);L.DomEvent.off(this._map._container,"touchend",this._onTouchEnd,this)},_onTouchStart:function(e){var touches=e.touches;if(touches.length!==2){return}this._touchStartTime=new Date},_onTouchEnd:function(e){var map=this._map,touches=e.changedTouches;if(!this._touchStartTime){return}L.DomEvent.preventDefault(e);if(new Date-this._touchStartTime<=L.Map.TwoFingerZoom.ZOOM_OUT_THRESHOLD){this._touchStartTime=null;map.zoomOut()}}});L.Map.addInitHook("addHandler","twoFingerZoom",L.Map.TwoFingerZoom);
